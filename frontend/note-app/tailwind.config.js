de3245d31eb345a8662be5a63bb1435f099bc0e4
RippedKek <tanjeeb1909@gmail.com>
2025-01-17 21:59:39 +0600

    Create Login Page

--- frontend/note-app/tailwind.config.js
+++ frontend/note-app/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // Colors used in the project
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 };
