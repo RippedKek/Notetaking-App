16fb1b95f15836c0f2481f1ba553c65a9f855c1c
RippedKek <tanjeeb1909@gmail.com>
2025-01-19 01:32:08 +0600

    Create NoteCard Component

--- frontend/note-app/src/pages/Home/Home.jsx
+++ frontend/note-app/src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <div className="container mx-auto">
+        <NoteCard
+          title="Meeting"
+          date="3rd April 2024"
+          content="ASDDDD"
+          tags="meeting"
+          isPinned={true}
+          onEdit={() => console.log("Edit")}
+          onDelete={() => console.log("Delete")}
+          onPinNote={() => console.log("Pin")}
+        />
+      </div>
     </>
   );
 };
