d286f7a4ac028ac1ff2e9902f0a70ff543330b45
RippedKek <tanjeeb1909@gmail.com>
2025-01-23 15:50:29 +0600

    Create Edit isPinned API

--- backend/index.js
+++ backend/index.js
@@ -213,6 +213,48 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
   }
 });
 
+// Update isPinned Value
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const isPinned = req.body;
+  const { user } = req.user;
+
+  if (!isPinned) {
+    return res.status(400).json({
+      error: true,
+      message: "No changes provided",
+    });
+  }
+  try {
+    const note = await Note.findOne({
+      _id: noteId,
+      userId: user._id,
+    });
+
+    if (!note) {
+      return res.status(400).json({
+        error: true,
+        message: "Note not found",
+      });
+    }
+
+    if (isPinned) note.isPinned = isPinned || false;
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note updated successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error",
+    });
+  }
+});
+
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
